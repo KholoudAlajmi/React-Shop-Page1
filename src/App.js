58f1ef066f4c10d5082c8f546759f16d711e0831
KholoudAlajmi <kholoudoa2000@gmail.com>
2024-11-10 22:43:36 +0300

    solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import bookImage from './image.jpg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App" >
+    
+<div className ="text-container">
+  <div className="App-link">
+<h1>Kholoud Pharmacy</h1>
+  <p>feel good inside, look good outside</p>
+
+  </div>
+  <img className = "storeImage" src = {bookImage}/>
+</div>
+      
+
     </div>
+    
+ 
+
+
+
+
   );
 }
 
