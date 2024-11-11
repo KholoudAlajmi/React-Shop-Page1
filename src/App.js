dacd601d67a8ec64d2286ad38640c803306fdf8f
KholoudAlajmi <kholoudoa2000@gmail.com>
2024-11-11 19:43:20 +0300

    solved3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 
 import './App.css';
 import bookImage from './image.jpg';
+import firstProduct from './BZBTSLOBIO77092_1.webp';
+import secondProduct from './31+tj+uVB+L._UL500_.jpg';
+import thirdProduct from './sunscreen.jpg';
+import fourthProduct from './cream.webp';
 function App() {
+  const products =[
+  {
+    image : firstProduct,
+    name: "bioderma face washer",
+    price:"12Kd",
+  },
+  {
+    image : secondProduct,
+    name:"makeup remover",
+    price:"4kd",
+  },
+  {
+    image:thirdProduct,
+    name:"Protection Face Sunscreen",
+    price:"6kd",
+  },
+  {
+    image: fourthProduct,
+    name:"QV cream",
+    price:"7,500kd",
+ },
+];
+
+
+const productsList = products.map((product, index) => {
+  return( <div key = {index}>
+    <div class="product-container">
+    <img class="product-image" src={product.image} ></img>
+    <p class="product-info">{product.name}</p>
+    <p class="product-info">{product.price}</p>
+    </div>
+ </div>)
+
+})
+
   return (
     <div className = "App" >
     
@@ -11,18 +50,19 @@ function App() {
   <p>feel good inside, look good outside</p>
 
   </div>
-  <img className = "storeImage" src = {bookImage}/>
+  <img className = "storeImage" src = {bookImage}></img>
 </div>
-      
-
-    </div>
-    
- 
 
 
+<div class="row">
 
+{productsList}
+
+</div>     
+</div> 
 
   );
 }
 
-export default App;
+
+export default App
